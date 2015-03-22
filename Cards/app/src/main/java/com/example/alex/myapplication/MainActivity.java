7069eaea8921df173cbf9267bf98b4b30359303b
Alexander <sasha@eventsoft.com.ua>
2015-03-22 13:09:09 +0200

    Works with activity

--- Cards/app/src/main/java/com/example/alex/myapplication/MainActivity.java
+++ Cards/app/src/main/java/com/example/alex/myapplication/MainActivity.java
@@ -5,21 +5,33 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import fragments.OnFragmentChanged;
+import models.CheckBoxModel;
 import models.Model;
+import models.MultyButtonsModel;
+import models.SinlgeTextModel;
+import models.UploadPhotoModel;
 
 
 public class MainActivity extends ActionBarActivity implements OnFragmentChanged {
 
     private List<Model> cardsModels;
     public Model currentCardModel;
+    private int currentCardPosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getQuestions();
+        setAdapter();
+    }
+
+    private void setAdapter() {
+        
     }
 
 
@@ -47,6 +59,48 @@ public class MainActivity extends ActionBarActivity implements OnFragmentChanged
 
     @Override
     public void setAnswer(boolean isAnswered, Model answeredModel) {
+        if(isAnswered){
+            removeQuestionSendResult(answeredModel);
+        }else {
+            nextQuestion(answeredModel);
+        }
+    }
 
+    private void removeQuestionSendResult(Model answeredModel) {
+        currentCardPosition = cardsModels.indexOf(answeredModel);
+        cardsModels.remove(answeredModel);
+
+        if(currentCardPosition > cardsModels.size()|| currentCardPosition == -1){
+            currentCardPosition = 0;
+        }
+        currentCardModel = cardsModels.get(currentCardPosition);
+    }
+
+    private void nextQuestion(Model answeredModel){
+        currentCardPosition = cardsModels.indexOf(answeredModel);
+        if(currentCardPosition > cardsModels.size()){
+            currentCardPosition = 0;
+        }else {
+            currentCardPosition++;
+        }
+
+        currentCardModel = cardsModels.get(currentCardPosition);
+    }
+
+    private void changeFragment(){
+
+    }
+
+    private void getQuestions(){
+        cardsModels = new ArrayList<>();
+        Model multiButtonsModel = new MultyButtonsModel();
+        Model checkBoxModel = new CheckBoxModel();
+        Model singleTextModel = new SinlgeTextModel();
+        Model uploadPhotoModel = new UploadPhotoModel();
+
+        cardsModels.add(multiButtonsModel);
+        cardsModels.add(checkBoxModel);
+        cardsModels.add(singleTextModel);
+        cardsModels.add(uploadPhotoModel);
     }
 }
